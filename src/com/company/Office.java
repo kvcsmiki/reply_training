eb45c20fd0ae4febd66da4c71979ff0cd8b826f7
kvcsmiki <kovacs.miklos.szabolcs@gmail.com>
2023-02-26 19:57:04 +0100

    office fix

--- src/com/company/Office.java
+++ src/com/company/Office.java
@@ -5,13 +5,11 @@ import java.util.ArrayList;
 public class Office {
 
     Desk[][] desks;
-    int width;
-    int height;
     ArrayList<Developer> developers;
     ArrayList<Manager> managers;
 
     public Office(int width, int height, ArrayList<Developer> developers, ArrayList<Manager> managers) {
-        Desk[][] desks = new Desk[width][height];
+        this.desks = new Desk[width][height];
         this.developers = developers;
         this.managers = managers;
     }
